6e32f401b80ce913ac6d0043e7146b70450dc273
João Vitor Vogel Vieira <joao0404200@gmail.com>
2023-11-24 17:00:17 -0300

    Primeira aula

---
+++ Aula01/index.js
@@ -0,0 +1,2 @@
+// Crie uma função que receba dois números e a operação aritmética que deseja realizar, retornando o resultado desta operação. A função deve conter testes unitários e tratamento de exceção. O parâmetro de operação deve conter validação e caso a operação repassada como argumento não for aceita, uma exceção deve ser lançada. Os valores repassados para a função devem ser convertidos em números e validados.
+
