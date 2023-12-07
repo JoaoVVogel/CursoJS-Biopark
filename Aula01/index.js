f33a344260f5ee711bd77c5f2ce945e1a69e8206
João Vitor Vogel Vieira <joao0404200@gmail.com>
2023-12-07 15:19:20 -0300

    aula 1

--- Aula01/index.js
+++ Aula01/index.js
@@ -1,36 +1,38 @@
 // Crie uma função que receba dois números e a operação aritmética que deseja realizar, retornando o resultado desta operação. A função deve conter testes unitários e tratamento de exceção. O parâmetro de operação deve conter validação e caso a operação repassada como argumento não for aceita, uma exceção deve ser lançada. Os valores repassados para a função devem ser convertidos em números e validados.
-var n1 = parseFloat(prompt('Digite o primeiro valor: '));
-var n2 = parseFloat(prompt('Digite o segundo valor: '));
-var operacao = prompt('Digite qual operação deseja realizar: ');
-console.log(n1,n2,operacao)
+var n1 = parseFloat(prompt("Digite o primeiro valor: "));
+console.log(n1)
+var operacao = prompt("Digite qual operação deseja realizar: ");
+console.log(operacao);
+var n2 = parseFloat(prompt("Digite o segundo valor: "));
+console.log(n2)
 
-function calculadora(n1,n2,operacao){
-var n1, n2, operacao, resultado;
+function calculadora(n1, n2, operacao) {
+  var n1, n2, operacao, resultado;
 
-switch(operacao){
-    case '+':
-        resultado = n1 + n2;
-        break;
-    
-    case '-':
-        resultado = n1 - n2;
-        break;
+  switch (operacao) {
+    case "+":
+      resultado = n1 + n2;
+      break;
 
-    case '/':
-        resultado = n1 / n2;
-        break;
+    case "-":
+      resultado = n1 - n2;
+      break;
 
-    case '*':
-        resultado = n1 * n2;
-        break;
-    
+    case "/":
+      resultado = n1 / n2;
+      break;
+
+    case "*":
+      resultado = n1 * n2;
+      break;
+
     default:
-        alert("Operação invalida!");
-        break;
-
-}
-return resultado;
+      alert("Operação invalida!");
+      break;
+  }
+  return resultado;
 }
 
 var resultado = calculadora(n1, n2, operacao);
-alert(`O resultado é ${resultado}`);
+alert(`Conta: ${n1} ${operacao} ${n2} = ${resultado}`)
+console.log(n1,operacao,n2,"=",resultado)
